1266e84127de157abe67fedabd5ab37b0c7d424f
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2021-01-08 23:43:47 +0100

    🐛 Use run_id instead of run_number [skip ci]

--- src/index.js
+++ src/index.js
@@ -160,7 +160,7 @@ const run = async () => {
 
 					---
 
-					This PR was created automatically by the [action-github-file-sync](https://github.com/BetaHuhn/action-github-file-sync) workflow run [#${ process.env.GITHUB_RUN_NUMBER || 0 }](https://github.com/${ GITHUB_REPOSITORY }/actions/runs/${ process.env.GITHUB_RUN_NUMBER || 0 })
+					This PR was created automatically by the [action-github-file-sync](https://github.com/BetaHuhn/action-github-file-sync) workflow run [#${ process.env.GITHUB_RUN_ID || 0 }](https://github.com/${ GITHUB_REPOSITORY }/actions/runs/${ process.env.GITHUB_RUN_ID || 0 })
 				`),
 				head: prBranch,
 				base: currentBranch
