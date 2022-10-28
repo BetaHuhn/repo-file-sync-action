a6715e48637ad12fb1a45aefe7ea481c8a1fcea7
Bruno Farina <bruno.ffarina@gmail.com>
2022-10-28 13:21:59 -0300

    Update index.js
    The set-output command is being deprecated, now it should be updated and used as setoutput.

--- dist/index.js
+++ dist/index.js
@@ -267,7 +267,7 @@ exports.getBooleanInput = getBooleanInput;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function setOutput(name, value) {
     process.stdout.write(os.EOL);
-    command_1.issueCommand('set-output', { name }, value);
+    command_1.issueCommand('setoutput', { name }, value);
 }
 exports.setOutput = setOutput;
 /**
