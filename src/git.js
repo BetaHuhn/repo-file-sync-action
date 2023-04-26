3e3b0ad60fc88ba2a6dd31aba324caad2e464698
Ken Odegard <kodegard@anaconda.com>
2023-04-25 23:12:27 -0500

    Switch to `git branch --show-current`

--- src/git.js
+++ src/git.js
@@ -122,7 +122,7 @@ export default class Git {
 
 	async getBaseBranch() {
 		this.baseBranch = await execCmd(
-			`git rev-parse --abbrev-ref HEAD`,
+			`git branch --show-current`,
 			this.workingDir
 		)
 	}
